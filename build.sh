511264afc30be0edcf5dfa7fbfbdcde7e43a0f99
Daniel Sturdivant <sturdivant20@gmail.com>
2025-01-16 15:59:19 -0600

    added config for specific receivers

--- build.sh
+++ build.sh
@@ -17,14 +17,14 @@ BoldMagenta='\033[1;35m'     # Purple
 BoldCyan='\033[1;36m'        # Cyan
 BoldWhite='\033[1;37m'       # White
 
-BUILDTYPE='Debug'
+BUILDTYPE='Release'
 C_COMPILER='clang-18'     # gcc
 CXX_COMPILER='clang++-18' # g++
 INSTALL_NAVTOOLS_EXAMPLES='False'
 INSTALL_SATUTILS_EXAMPLES='False'
 INSTALL_STURDIO_EXAMPLES='False'
 INSTALL_STURDINS_EXAMPLES='False'
-INSTALL_STURDR_EXAMPLES='True'
+INSTALL_STURDR_EXAMPLES='False'
 
 clear
 rm -r build
